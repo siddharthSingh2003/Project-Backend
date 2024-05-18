9362b4aed8092c159317df4abe2addcd1155ad4c
sidddharthsingh <sidsingh1679@gmail.com>
2024-05-18 16:43:38 +0530

    making connection 1

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+
+(async () => {
+    try {
+        await mongoose.conect(`${process.env.MONGODB_URI}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.nextTick.prototype, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+    }
+})()
+*/
