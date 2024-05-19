a30df4a2a6e306527b597ae9168b12ef804423c2
sidddharthsingh <sidsingh1679@gmail.com>
2024-05-19 17:32:47 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -6,8 +6,8 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
-    } catch (error) {
-        console.log(("MONGODB connection Failed", error));
+    } catch (err) {
+        console.log(("MONGODB connection Failed", err));
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path :'./env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port :${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+    console.log("MONGO db connection failed !!", err)
+})
 
 
 
