a4fb0ca58cbd4be637502c653d305aefaa622de2
sidddharthsingh <sidsingh1679@gmail.com>
2024-05-22 14:51:47 +0530

    avatar await bug , complete server testing done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,35 +1,31 @@
-import { v2 as cloudinary } from "cloudinary"
-
+import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
-         
+
+
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key:  process.env.CLOUDINARY_API_KEY, 
-  api_secret:  process.env.CLOUDINARY_API_SECRET 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
-
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) return null;
+        if (!localFilePath) return null
         //upload the file on cloudinary
-        cloudinary.uploader.upload(localFilePath, {
-            resource_type:"auto"
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         })
-        //file uploadede successfully 
-        console.log(("file is uploaded on cloudinary",
-            response.url));
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
-        fs.unlinkSync(localFilePath) //remove the localy saved temporally file as the upload opration got failed
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
+
+
+
 export {uploadOnCloudinary}
-
-
-
-// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//   { public_id: "olympic_flag" }, 
-//   function(error, result) {console.log(result); });
