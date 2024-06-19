c62ed4ed302390191e60bc93cace81f4787522fc
sidddharthsingh <sidsingh1679@gmail.com>
2024-06-19 16:09:49 +0530

    get playlist by id

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -36,6 +36,19 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Playlist required")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+        throw new ApiError(400, "playlist not found");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, playlist, "Playlist fetched successfully"))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
